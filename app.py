bea7d21730ff4ffb20810b30d1d6e8dce4e04e38
NamrataIngle <121537956+NamrataIngle@users.noreply.github.com>
2023-06-01 23:42:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,50 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 24 13:01:17 2020
-
-"""
-
-
-
 import pandas as pd
 import streamlit as st 
-from sklearn.linear_model import LogisticRegression
-from pickle import dump
-from pickle import load
+from textblob import TextBlob
 
-st.title('Model Deployment: Logistic Regression')
+st.title('Model Deployment: Sentiment Analysis')
+text = st.text_area("Please Enter your text")
 
-st.sidebar.header('User Input Parameters')
-
-def user_input_features():
-    CLMSEX = st.sidebar.selectbox('Gender',('1','0'))
-    CLMINSUR = st.sidebar.selectbox('Insurance',('1','0'))
-    SEATBELT = st.sidebar.selectbox('SeatBelt',('1','0'))
-    CLMAGE = st.sidebar.number_input("Insert the Age")
-    LOSS = st.sidebar.number_input("Insert Loss")
-    data = {'CLMSEX':CLMSEX,
-            'CLMINSUR':CLMINSUR,
-            'SEATBELT':SEATBELT,
-            'CLMAGE':CLMAGE,
-            'LOSS':LOSS}
-    features = pd.DataFrame(data,index = [0])
-    return features 
-    
-df = user_input_features()
-st.subheader('User Input parameters')
-st.write(df)
-
-
-# load the model from disk
-loaded_model = load(open('Logistic_Model.sav', 'rb'))
-
-prediction = loaded_model.predict(df)
-prediction_proba = loaded_model.predict_proba(df)
-
-st.subheader('Predicted Result')
-st.write('Yes' if prediction_proba[0][1] > 0.5 else 'No')
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
-
-
+if st.button("Analyze the Sentiment"):
+  blob = TextBlob(message)
+  result = blob.sentiment
+  polarity = result.polarity
+  subjectivity = result.subjectivity
+  if polarity < 0:
+    st.warning("The entered text has negative sentiments associated with it"+str(polarity))
+    rain(
+    emoji="????",
+    font_size=20,  # the size of emoji
+    falling_speed=3,  # speed of raining
+    animation_length="infinite",  # for how much time the animation will happen
+)
+  if polarity >= 0:
+    st.success("The entered text has positive sentiments associated with it."+str(polarity))
+    rain(
+    emoji="????",
+    font_size=20,  # the size of emoji
+    falling_speed=3,  # speed of raining
+    animation_length="infinite",  # for how much time the animation will happen
+    )
+  st.success(result)
