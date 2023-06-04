64a70d98f5fab44aacf60b0d75b33a2c6d938a6b
NamrataIngle <121537956+NamrataIngle@users.noreply.github.com>
2023-06-04 14:24:51 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -14,6 +14,7 @@ import nltk
 nltk.download('all')
 from nltk.corpus import stopwords
 from wordcloud import WordCloud
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(max_features=200)
 
