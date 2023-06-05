1a1c8dd829e384712594d1e4787c25403b1b2974
NamrataIngle <121537956+NamrataIngle@users.noreply.github.com>
2023-06-05 22:58:09 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -7,89 +7,71 @@ This is a temporary script file.
 
 import streamlit as st
 import pickle
-import textblob
-import pandas as pd
-#from cleantext import clean
-#from tensorflow.keras.preprocessing.text import Tokenizer
-import time
 import re
 import nltk
-from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 nltk.download('stopwords')
-#from wordcloud import WordCloud
+nltk.download('punkt')
+from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
+#import pandas as pd
+#from prediction import predict
 
-file = 'clf.pkl'
-#model = pickle.load(file)
+file = open('clf.pkl','rb')
+model = pickle.load(file)       
 
-with open(file, 'rb') as file:
-    model = pickle.load(file)
+vectoriser1 = TfidfVectorizer(max_features=500)
 
-text = ''
-text1 = ''
 
-st.header('Sentiment Analysis \U0001F606')
-with st.title('Analyze Text'):
-	text = st.text_input('Text here: ')
+def preprocess_tweet(tweet):
+    # Remove any URLs
+    tweet = re.sub(r'http\S+', '', tweet)
+    # Remove any non-alphanumeric characters
+    tweet = re.sub(r'[^\w\s]', '', tweet)
+    # Remove any digits
+    tweet = re.sub(r'\d+', '', tweet)
+    # Remove any mentions
+    tweet = re.sub(r'@\w+', '', tweet)
     
-if text:
-	text1=text
-	blob = TextBlob(text)
+    # Tokenization
+    tokens = nltk.word_tokenize(tweet)
     
-def Analyse(text):
-    prediction = model.predict('class')
-    return prediction
-
-def display_sarcastic_remark(remark):
-    st.title(remark)
-    time.sleep(0.1)
-
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token.lower() not in stop_words]
     
-
-
-#plt.figure(figsize = (20,20))
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
     
-if(text1!=""):
-    st.title("Cleaned Text")
-    text1 = re.sub('((www.[^s]+)|(https?://[^s]+))|(http?://[^s]+)', '',text1)
-    tknzr = TweetTokenizer(strip_handles=True)
-    text1=tknzr.tokenize(text1)
-    text1=str(text1)
-    text1=re.sub(r'[^a-zA-Z0-9\s]', '', text1)
-    #text1=clean(text1, clean_all= False, extra_spaces=True ,stopwords=True ,lowercase=True ,numbers=True , punct=True)
-    st.write(text1)
+    return ' '.join(tokens)
+
+def main():
+    # Set the page title
+    st.title("Sentiment Analysis")
+    st.markdown('Toy model to play to classify text into(Figurative,Irony,Regular,Sarcasm)')
     
-with open(file, 'rb') as file:
-    model = pickle.load(file)
-unseen_tweets=[text]
-unseen_df=pd.DataFrame(unseen_tweets)
-unseen_df.columns=["Unseen"]
-
-vectorizer = TfidfVectorizer(max_features=200)
-
-stopwords_set = set(stopwords.words('english'))
-
-X_test = vectorizer.transform(unseen_tweets)
-y_pred = model.predict(X_test)
-
-
-if text!="":
-    if(y_pred==0):
-        remark = "That's Figurative!😄"
-        display_sarcastic_remark(remark)
-    if(y_pred==1):
-        remark = "That's Irony!😏"
-        display_sarcastic_remark(remark)
-    if(y_pred==2):
-        remark = "That's Regular!😐"
-        display_sarcastic_remark(remark)
-    if(y_pred==3):
-        remark = "That's Sarcasm!🙃"
-        display_sarcastic_remark(remark)
-else:
-    st.write(text1)
-    remark = "No Words to Analyze"
-    display_sarcastic_remark(remark)
+    # Get user input
+    tweet_input = st.text_input("Text here:")
+    
+    if st.button("Analyse text"):
+        if tweet_input:
+            # Preprocess the input tweet text
+            preprocessed_text = preprocess_tweet(tweet_input)
+            
+            # Apply TF-IDF transformation
+            tfidf = vectoriser1.transform([preprocessed_text])
+            
+            # Make prediction using the trained classifier
+            prediction = model.predict(tfidf)[0]
+            
+            # Display the prediction
+            st.write("Prediction:", prediction)
+        else:
+            st.write("No text to analyse")
+    
+if __name__ == "__main__":
+    main()
+    
 
     
